2203ebc235ccdf850954034139466055d4a98d71
root <root@DESKTOP-VEND2NU>
2023-03-16 17:22:53 +0100

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+
+{
+
+	printf("size of a char: %lu byte(s)", size(char));
+	printf("size of int: %lu bytes(s)", sizeof(int));
+	printf("size of a long int: %lu bytes", sizeof(long loni int));
+	printf("size of a float: %lu bytes(s)", sizeof(float));
+	returfn (0);
+}
