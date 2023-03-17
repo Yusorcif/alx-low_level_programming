7caecddc24ded6884a4833928f8ece2a4a7de8a8
root <root@DESKTOP-VEND2NU>
2023-03-17 16:42:00 +0100

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,8 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - main function
- * Discription: 'get the last degit of a number'
+ * main - Entry point
+ * Discription: 'get the last digit of a number'
  * Return: always 0
  */
 int main(void)
