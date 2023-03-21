be2abf3f3451c89293e26cc34def2e7cbcbc6681
root <root@DESKTOP-VEND2NU>
2023-03-22 00:13:40 +0100

    lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - function to lower case
+ *
+ * Return: 1 if it a lower case
+ * and  0 otherwise
+ */
+
+int _islower(int c);
+{
+	if (c >= 97 && c <= 122)
+		return (i);
+	else
+		return (0);
+}
