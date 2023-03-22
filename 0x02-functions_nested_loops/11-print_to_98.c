624d0ba5e4dcf6ac4a4e9c1b10894a42be89d95d
root <root@DESKTOP-VEND2NU>
2023-03-22 21:21:22 +0100

    print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 
 	if (n >= 0 && n <= 98)
 	{
-		for (i = n ; i <= 98 ; i++);
+		for (i = n ; i <= 98 ; i++)
 		{
 			printf("%d, ", i);
 		}
@@ -30,7 +30,7 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		for (i - n ; i >= 98 ; i--)
+		for (i = n ; i >= 98 ; i--)
 		{
 			printf("%d, ", i);
 		}
