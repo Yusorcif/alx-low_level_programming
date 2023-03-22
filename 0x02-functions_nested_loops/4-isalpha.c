41645d1070b8ec76c5ef14c866c0b1347de4869e
root <root@DESKTOP-VEND2NU>
2023-03-22 17:59:43 +0100

    print alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,16 @@
 #include "main.h"
+/**
+ * _isalpha - function that prints alphabets
+ *
+ * @c: parameter to be checked
+ *
+ * Return: 1 if it is an alphabet
+ */
 
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122 || (c >= 65 && c <= 90))
-			return (1);
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
 	else
-	return (0);
+		return (0);
+}
