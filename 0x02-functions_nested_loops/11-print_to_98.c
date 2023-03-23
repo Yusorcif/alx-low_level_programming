bfc19add0691230f557e41130cb70a3f850f6c5c
root <root@DESKTOP-VEND2NU>
2023-03-23 05:06:48 +0100

    print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,6 +8,7 @@
  *
  * Return: always 0
  */
+
 void print_to_98(int n)
 {
 	int i;
@@ -16,7 +17,9 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ")
 		}
 		printf("\n");
 	}
@@ -24,7 +27,9 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				 printf(", ")
 		}
 		printf("\n");
 	}
@@ -32,11 +37,13 @@ void print_to_98(int n)
 	{
 		for (i = n ; i >= 98 ; i--)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				 printf(", ")
 		}
 	printf("\n");
 	}
 	else if (n == 98)
-		printf("%d, ", n);
+		printf("%d", n);
 
 }
