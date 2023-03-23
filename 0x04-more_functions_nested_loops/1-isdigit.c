107f6603e7de2debd90ad2c6be0d9428dea03090
root <root@DESKTOP-VEND2NU>
2023-03-23 16:55:38 +0100

    function that print 0 and 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * int _isdigit - function that checks for a digit
+ *
+ * @c: function parameter
+ * Return: 0 or 1
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
