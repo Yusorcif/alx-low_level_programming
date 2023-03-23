698da9cc1c693b41d0a06b85ba03085c02fef6e1
root <root@DESKTOP-VEND2NU>
2023-03-23 17:03:11 +0100

    print a and b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * int mul - function that prints mul
+ *
+ * @a: function parameter
+ *
+ * @b: function parameter
+ *
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
