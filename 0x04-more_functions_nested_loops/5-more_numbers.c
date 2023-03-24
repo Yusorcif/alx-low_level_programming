36309aa3e7b549a2e9a9e61e0fc0665c538dc634
root <root@DESKTOP-VEND2NU>
2023-03-24 12:19:17 +0100

    diagonal

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,24 +1,32 @@
+
 #include "main.h"
+
 /**
- * more_numbers - function thta prints 10 times
- * Return: 10 times the number
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ *
+ * followed by a new line
  */
 
 void more_numbers(void)
-{
-	int x, y;
 
-	for (x = 0; x < 10; x++)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
 	{
-	for (y = 0; y <= 14; y++)
+
+	for (j = 0; j < 15; j++)
+
 	{
-	if (y >= 10)
-	{
-	_putchar(y / 10 + 0);
-	}
-	_putchar(y % 10 + 0);
+
+	if (j >= 10)
+
+	_putchar(j / 10 + '0');
+
+	_putchar(j % 10 + '0');
+
 	}
 	_putchar('\n');
 	}
-}
 
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_J
+#define MAIN_H
 
 int _putchar(char c);
 int _isupper(int c);
