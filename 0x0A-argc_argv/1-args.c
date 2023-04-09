15bbe9388181920c720cfb66e1d586ffaea6957b
root <root@DESKTOP-VEND2NU>
2023-04-09 08:43:51 +0100

    a program that prints the number of arguments passed into it

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,9 @@
  * @argv: argument array
  * Return: always 0
  */
-int main(int argc, char *argv[] __attribute__ ((unused)))
+int main(int argc, char *argv[])
 {
+	(void) argv; /* ignore*/
 	printf("%d\n", argc);
 
 return (0);
